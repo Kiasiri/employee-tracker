def0c9f9ddb9e96aebb956fecabe3fb3616ae993
Matthew Wiessing <matthewwiessing@gmail.com>
2021-10-17 18:38:09 -0500

    made constructors for employee department and role

---
+++ constructors.js
@@ -0,0 +1,22 @@
+function Employee(fname, lname, role, manager) {
+  this.first_name = fname;
+  this.last_name = lname;
+  this.role_id = role;
+  this.manager = manager;
+}
+
+function Department(name) {
+  this.name = name;
+}
+
+function Role(title, salary, department) {
+  this.title = title;
+  this.salary = salary;
+  this.department_id = department;
+}
+
+module.exports = {
+  Employee: Employee,
+  Department: Department,
+  Role: Role,
+};
