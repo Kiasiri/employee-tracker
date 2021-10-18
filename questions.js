b63b392eef8c7375a4ea6c0550ce2cb57de30125
Matthew Wiessing <matthewwiessing@gmail.com>
2021-10-17 22:21:51 -0500

    added questions for adding an employee

--- questions.js
+++ questions.js
@@ -13,11 +13,45 @@ const selectionChoices = [
   "Exit",
 ];
 
-const employeeAction = [
+const choose = [
   {
     name: "selection",
     type: "list",
     message: "What would you like to do?",
     choices: selectionChoices,
+  },
+];
+
+const addEmployeeQuestions = [
+  {
+    name: "fname",
+    type: "input",
+    message: "What is the employee's first name?",
+  },
+  {
+    name: "lname",
+    type: "input",
+    message: "What is the employee's last name?",
+  },
+  {
+    name: "department",
+    type: "input",
+    message: "What is the employee's department?",
+  },
+  {
+    name: "role",
+    type: "input",
+    message: "What role will the employee assume?",
+  },
+  {
+    name: "salary",
+    type: "input",
+    message: "What is the employee's salary?",
+  },
+  {
+    name: "manager",
+    type: "list",
+    message: "Who is the employee's manager?",
+    choices: getManagers,
   },
 ];
